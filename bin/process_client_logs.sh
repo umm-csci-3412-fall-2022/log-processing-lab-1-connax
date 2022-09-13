d25d340c1718f02f58c9f41cb50533a0602fa93e
Max Quintavalle <quint218@normandy.morris.umn.edu>
2022-09-13 14:09:39 -0500

    Finished writing process_client_logs making it so it loops through all the tgz files and makes a directory the files it loops through.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,30 +3,33 @@
 scratch=$(mktemp --directory)
 
 #We want to loop this later
-tgzfile=../log_files/ganesha_secure.tgz
-
-tgzdirname= $(awk 'match($0, /([\w+]+)_secure\.tgz$/, name) {print name[1]}' < "$tgzfile")
-
-echo "$tgzdirname"
-
-pushd "$scratch"
-
-mkdir ganesha
-
-popd
-
-tar -xvf "$tgzfile" --directory "$scratch"/ganesha
 
 touch failed_login_data.txt
 
-for logfile in "$scratch"/ganesha/var/log/*
+for tgzfile in ../log_files/*
 
 do
 
-	echo "$logfile"
+	tgzdirname=$(basename -s _secure.tgz $tgzfile)
 
-	awk 'match($0, /([a-zA-Z]+\s[0-9]+\s[0-9]+):.+Failed\spassword.+user\s(\w+)\sfrom\s(\S+)/, groups) {print  groups[1] " " groups[2] " " groups[3]}' < "$logfile" > failed_login_data.txt
+	echo "$tgzdirname"
 
+	pushd "$scratch"
+
+	mkdir "$tgzdirname"
+
+	popd
+
+	tar -xvf "$tgzfile" --directory "$scratch"/"$tgzdirname"
+
+	for logfile in "$scratch"/"$tgzdirname"/var/log/*
+
+	do
+
+		echo "$logfile"
+
+		awk 'match($0, /([a-zA-Z]+\s[0-9]+\s[0-9]+):.+Failed\spassword.+user\s(\w+)\sfrom\s(\S+)/, groups) {print  groups[1] " " groups[2] " " groups[3]}' < "$logfile" > failed_login_data.txt
+
+	done
 done
-
 rm -rf "$scratch"
