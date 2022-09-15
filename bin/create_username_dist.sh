98dfdb9cd9521a32ea1f52ee9bd369fda5126e25
Conner <hetti031@morris.umn.edu>
2022-09-15 15:25:32 -0500

    worked on username_dist.html files

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -4,9 +4,13 @@
 # For now we assume this is the directory of the computer name, not the directory with all the computer names
 dirname=$1
 
-cd "$dirname"
+pushd "$dirname"
 
-touch username_dist.html
+touch username.html
 
-awk 'match($0, /(\S+) \S+$/, group) {print group[1]}' < failed_login_data.txt | sort | uniq -c | awk 'match($0, /([0-9]+) (\S+)/, group){print "data.addrow([\x27" group[2] "\x27,"group[1] "]);"}'>> username_dist.html
+awk 'match($0, /(\S+) \S+$/, group) {print group[1]}' < failed_login_data.txt | sort | uniq -c | awk 'match($0, /([0-9]+) (\S+)/, group){print "data.addRow([\x27" group[2] "\x27,"group[1] "]);"}'>> username.html
 
+popd
+
+
+./bin/wrap_contents.sh "$dirname"/username.html html_components/username_dist $dirname/username_dist.html
