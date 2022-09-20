6599b1ef56db844c1ff7f3912082f4e53d8d86a2
Kahnurd <hetti031@morris.umn.edu>
2022-09-20 17:10:44 -0500

    Change create_username_dist.sh to loop through each directory in given directory
    Added a for loop to go through each directory in the given directory and
    put all of the usernames into a "listofusers.txt" and then sorted it,
    counted it, and then used awk to format the main content for
    username.html (which is the list of data.addRow[]s). Then combined the
    header, footer, and middle content into a file called
    username_dist.html.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -4,13 +4,23 @@
 # For now we assume this is the directory of the computer name, not the directory with all the computer names
 dirname=$1
 
-pushd "$dirname"
+#cd "$dirname"
 
+touch listofusers.txt
 touch username.html
 
-awk 'match($0, /(\S+) \S+$/, group) {print group[1]}' < failed_login_data.txt | sort | uniq -c | awk 'match($0, /([0-9]+) (\S+)/, group){print "data.addRow([\x27" group[2] "\x27,"group[1] "]);"}'>> username.html
+for compdir in "$dirname"/*
 
-popd
+do
+awk 'match($0, /(\S+) \S+$/, group) {print group[1]}' < ${compdir}/failed_login_data.txt >> listofusers.txt
+done
+
+sort listofusers.txt | uniq -c | awk 'match($0, /([0-9]+) (\S+)/, group){print "data.addRow([\x27" group[2] "\x27,"group[1] "]);"}'>> ./username.html
 
 
-./bin/wrap_contents.sh "$dirname"/username.html html_components/username_dist $dirname/username_dist.html
+
+
+./bin/wrap_contents.sh username.html html_components/username_dist "$dirname"/username_dist.html
+
+rm listofusers.txt
+rm username.html
