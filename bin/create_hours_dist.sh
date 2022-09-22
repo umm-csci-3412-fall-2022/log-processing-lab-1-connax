b030094ccef1caa7e6f8a30a5d40415d449ac92b
Conner <hetti031@morris.umn.edu>
2022-09-22 15:55:01 -0500

    The error that we were dealing with came from the awk statement in the process_client_logs.sh that didn't catch the spaces correctly

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -8,13 +8,10 @@ touch hours.html
 for compdir in "$dirname"/*/
 
 do
-awk 'match($0, /\S+ \S+ ([0-9]+)/, group) {print group[1]}' < ${compdir}/failed_login_data.txt >> listofhours.txt
+awk 'match($0, /\S+ \S+ ([0-9]{2})/, group) {print group[1]}' < ${compdir}/failed_login_data.txt >> listofhours.txt
 done
 
 sort listofhours.txt | uniq -c | awk 'match($0, /([0-9]+) (\S+)/, group){print "data.addRow([\x27" group[2] "\x27, "group[1] "]);"}'>> ./hours.html
-
-
-
 
 ./bin/wrap_contents.sh hours.html html_components/hours_dist "$dirname"/hours_dist.html
 
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -10,7 +10,7 @@ for logfile in  "$tgzdirname"/var/log/*
 do
 
 
-	awk 'match($0, /(.+):[0-9]+:[0-9]+.+Failed\spassword\s.+\s(\w+)\sfrom\s(\S+)\sport.+$/, groups) {print groups[1] " " groups[2] " " groups[3]}' < "${logfile}" >> failed_login_data.txt
+	awk 'match($0, /(\S+)\s+(\S+) (\S+):[0-9]+:[0-9]+.+Failed\spassword\s.+\s(\S+)\sfrom\s(\S+)\sport.+$/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4] " " groups[5]}' < "${logfile}" >> failed_login_data.txt
 
 done
 
